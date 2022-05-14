48cf1a9bdceca590127e3d6a6f49b0d271958ad6
MariqueRobin <77294393+MariqueRobin@users.noreply.github.com>
2022-05-14 10:58:14 +0200

    create employe page/component

---
+++ src/pages/Employee.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Employee = () => {
+    return (
+        <div>
+            Empl
+            {/* <Employee name='Julien' /> */}
+        </div>
+    );
+}
+
+export default Employee;
