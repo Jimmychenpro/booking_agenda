c5cf2933fdc660be8dea1a1c0583212c7ca37fff
JimmyChen6395 <jimmychen.pro@gmail.com>
2022-05-21 10:53:46 +0200

    update

--- src/pages/Booking.js
+++ src/pages/Booking.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
+function Booking() {
+  const [value, onChange] = useState(new Date());
 
-
-const Booking = () => {
-    return (
-        <div>
-            <h1>Booking</h1>
-
+  return (
+    <>
+        <h1>Choisissez une date : </h1>
+        <div className='calendar'>
+            <Calendar onChange={onChange} value={value} />
         </div>
-
-    );
+        <div className='Hours'>
+            <h1>Sélectionnez une heure :</h1>
+            <div>10:00</div>
+        </div>
+    </>
+  );
 }
 
 export default Booking;
